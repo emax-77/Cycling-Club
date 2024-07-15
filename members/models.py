82fe6ba5e584d92da90e5ccea611fab7e1fca6ef
emax-77 <peter.wirth@gmail.com>
2024-07-15 13:01:18 +0200

    added new columns into Member table

--- members/models.py
+++ members/models.py
@@ -3,8 +3,12 @@ from django.db import models
 class Member(models.Model):
   firstname = models.CharField(max_length=255)
   lastname = models.CharField(max_length=255)
-  phone = models.IntegerField(null=True)
+  phone = models.CharField(max_length=255, null=True)
   joined_date = models.DateField(null=True)
-
+  address = models.CharField(max_length=255,null=True)
+  city = models.CharField(max_length=255,null=True)
+  country = models.CharField(max_length=255,null=True)
+  member_fees = models.IntegerField(null=True)
+  
   def __str__(self):
     return f"{self.firstname} {self.lastname}"
