e6167e6d2cd8d4d8a7dd45e10bca4bf087cdfc4b
emax-77 <peter.wirth@gmail.com>
2024-08-01 16:57:30 +0200

    updates

--- my_ebike/urls.py
+++ my_ebike/urls.py
@@ -20,10 +20,10 @@ from debug_toolbar.toolbar import debug_toolbar_urls
 from django.conf.urls.static import static 
 from django.conf import settings
 
-#import debug_toolbar
+import debug_toolbar
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('members.urls')),
-   # path('__debug__/', include(debug_toolbar.urls)),
+    path('__debug__/', include(debug_toolbar.urls)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + debug_toolbar_urls()
