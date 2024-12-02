c548431b67d55b7250decef7126d9ab6c1892edb
Peter Wirth <57559346+emax-77@users.noreply.github.com>
2024-12-02 10:34:52 +0100

    static file fix on urls.py

--- my_ebike/urls.py
+++ my_ebike/urls.py
@@ -27,8 +27,8 @@ urlpatterns = [
     path('', include('authentication.urls')),
     path('', include('members.urls')),
     path('__debug__/', include(debug_toolbar.urls)),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + debug_toolbar_urls()
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + debug_toolbar_urls()
 
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+""" if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) """
